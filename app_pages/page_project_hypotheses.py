6cb679ad31463ef6f1ead77998ee68b8c6595cc2
olala2024 <olya.burtseva@gmail.com>
2025-02-22 22:46:47 +0100

    Edit styling of hypothesis project page

--- app_pages/page_project_hypotheses.py
+++ app_pages/page_project_hypotheses.py
@@ -1,9 +1,14 @@
 import streamlit as st
 
-
 def page_project_hypotheses_body():
-    st.title("Project Hypotheses")
-    
+    st.markdown(
+        """
+        * [Hypothesis 1: Higher Overall Quality Leads to Higher Sale Prices](#hypothesis-1-higher-overall-quality-leads-to-higher-sale-prices)
+        * [Hypothesis 2: Larger Living Areas Result in Higher Sale Prices](#hypothesis-2-larger-living-areas-result-in-higher-sale-prices)
+        * [Hypothesis 3: Newer Houses Have Higher Sale Prices](#hypothesis-3-newer-houses-have-higher-sale-prices)
+        """
+    )
+
     st.write("### Hypothesis 1: Higher Overall Quality Leads to Higher Sale Prices")
     st.write("**Hypothesis**: Houses with higher overall quality ratings (`OverallQual`) tend to have higher sale prices.")
     st.write("**Validation**: Perform a correlation analysis between `OverallQual` and `SalePrice`. Create scatter plots and calculate the Pearson and Spearman correlation coefficients to validate the strength and direction of the relationship.")
