d788ac126acc960d3e20644f833be4f4bfc0342f
olala2024 <olya.burtseva@gmail.com>
2025-02-23 01:39:39 +0100

    Edit page model performance page

--- app_pages/page_model_performance.py
+++ app_pages/page_model_performance.py
@@ -1,6 +1,29 @@
 import streamlit as st
 
-
 def page_model_performance_body():
     st.title("Regression Model Performance Metrics")
-    st.write("Summary of model performance and metrics")
+        
+    # Regression Model Description
+    st.write("""
+   
+    **Model Success Metrics**:
+    - The model will be considered successful if it achieves at least 70% R² score on the test set.
+
+    **Model Failure Criteria**:
+    - The model will be considered a failure if it fails to achieve a 70% R² score on the test set.
+
+    **Model Output**:
+    - The model output is defined as the predicted sale price of a house based on its attributes.
+    """)
+    
+    # Display Mean Squared Error (MSE)
+    st.header("Mean Squared Error (MSE)")
+    st.write("Training Set MSE: 411,496,843.69")
+    st.write("Testing Set MSE: 2,299,035,504.57")
+    
+    # Display R² Score
+    st.header("R² Score")
+    st.write("Training Set R² Score: 0.9310")
+    st.write("Testing Set R² Score: 0.7003")
+    
+    
